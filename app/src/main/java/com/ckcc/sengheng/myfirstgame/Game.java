ad295a2b6d9b048d8d60164d8869860f246700c4
sengheng <hongsengheng@gmail.com>
2016-10-14 08:48:20 -0700

    full code

--- app/src/main/java/com/ckcc/sengheng/myfirstgame/Game.java
+++ app/src/main/java/com/ckcc/sengheng/myfirstgame/Game.java
@@ -1,13 +1,28 @@
 package com.ckcc.sengheng.myfirstgame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class Game extends AppCompatActivity {
+
+public class Game extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
     }
+
+
+
 }
